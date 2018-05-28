f9a5d57ad17a42d90dba1f3a308f40238d8e56f4
Micha den Heijer <2014071@hvc.nl>
2018-05-28 14:32:45 +0000

    I think it's working I just tested it working I just saw it working It'll not work probably

---
+++ assets/js/pauseVideo.js
@@ -0,0 +1,18 @@
+var pauseTime = document.getElementById('intro-video').dataset.pause1;
+var video = document.getElementById('intro-video');
+var question = document.getElementById('questions');
+
+question.style.display = 'none';
+
+var pausing_function = function(){
+    if(this.currentTime >= pauseTime) {
+        this.pause();
+
+        // remove the event listener after you paused the playback
+        this.removeEventListener("timeupdate",pausing_function);
+        question.style.display = 'inline';
+        video.style.display = 'none';
+    }
+};
+
+video.addEventListener("timeupdate", pausing_function);
