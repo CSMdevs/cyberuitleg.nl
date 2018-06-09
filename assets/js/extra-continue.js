74d800d0a64683279c7b39d6cdfa3d1ac02438ad
Micha den Heijer <2014071@hvc.nl>
2018-06-09 17:47:41 +0000

    jquery used

--- assets/js/extra-continue.js
+++ assets/js/extra-continue.js
@@ -3,6 +3,6 @@ var meerVertellenButton = document.getElementById("antwoord-meer-verder");
 
 meerVertellen.style.display = 'none';
 
-meerVertellen.onclick = function (){
-    meerVertellen.style.display = 'inline';
+meerVertellenButton.onclick = function (){
+     $(meerVertellen).show("slow");
 }
