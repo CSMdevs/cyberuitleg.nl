6c9a79a0dbf89a8d6c40747c5b9f3b47c41103e0
Micha den Heijer <2014071@hvc.nl>
2018-06-09 17:44:12 +0000

    tst

---
+++ assets/js/extra-continue.js
@@ -0,0 +1,8 @@
+var meerVertellen = document.getElementById("meer-vertellen-gwn");
+var meerVertellenButton = document.getElementById("antwoord-meer-verder");
+
+meerVertellen.style.display = 'none';
+
+meerVertellen.onclick = function (){
+    meerVertellen.style.display = 'inline';
+}
