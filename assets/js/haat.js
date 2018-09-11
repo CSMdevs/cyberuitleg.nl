c6e47ea6b236102b7759664e026eef503c8d2985
Micha den Heijer <micha@michadenheijer.nl>
2018-09-11 11:45:19 +0000

    micha has to fix everything cemre is just not so smart stan is just stupid we also have patrick but i dont have to explain anything

--- assets/js/haat.js
+++
@@ -1,6 +0,0 @@
-
-  window.onbeforeunload = confirmExit;
-  function confirmExit()
-  {
-    return "Weet je zeker dat je deze website wilt verlaten?";
-  }
--- assets/js/miniserie-quiz.js
+++ assets/js/miniserie-quiz.js
@@ -7,41 +7,8 @@ function demoFunction() {
   
   if(test === false) {
     //melding als er een kommagetal is
-    document.getElementById("demo").innerHTML = "Jouw getal is geen geheel getal, dit kan dus niet goed zijn!";
+    document.getElementById("demo").innerHTML = "Het ingevulde antwoord is geen geheel getal, dit kan dus niet goed zijn!";
+  } else {
+    document.getElementById("demo").innerHTML = null;
   }
 }
-
-
-/* function check(z) {
-    if (Number.isInteger(document.getElementById("vraag1"))) { 
-        return "Jouw antwoord voldoet aan de eisen";
-    }
-    return "Het getal mag enkel bestaan uit de nummers 1 en 0";
-}
-
-console.log(check(document.getElementById("vraag1")));
-console.log(check(document.getElementById("vraag1"))); */
-/*
-setInterval(
-
-, 1000);
-*/
-
-
-/* setInterval(function check(z) {
-  if (Number.isInteger(z)) {
-    return 'Jouw getal is een geheel getal!';
-  }
-  return 'Jouw getal is geen geheel getal, dit kan dus niet goed zijn!';
-}, 1000);
-
-document.getElementById("demo").innerHTML = check(document.getElementById("vraag1")[0].value); */
-
-//console.log(check(10));
-// expected output: "Fits!"
-
-// console.log(check(5));
-// expected output: "Does NOT fit!"
-
-
-//console.log(demoFunction(document.getElementById("vraag1")));
