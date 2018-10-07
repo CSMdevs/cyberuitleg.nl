6b209d0769aca8c84bfb8f270b9c667102f287de
Micha den Heijer <micha@michadenheijer.nl>
2018-10-07 17:47:59 +0000

    more delay

--- assets/js/end-animations.js
+++ assets/js/end-animations.js
@@ -4,6 +4,6 @@ function sleep(delay) {
         var start = new Date().getTime();
         while (new Date().getTime() < start + delay);
       }
-    sleep (1000);
+    sleep (5000);
     $("body").removeClass("fade-out");
 });
